e6dba61da741e38316441dab46e6e744e1c7b1ed
sarra <sarramseibi2@gmail.com>
2025-10-20 15:51:31 +0100

    lab3

--- glsi/src/Controller/EtudiantController.php
+++ glsi/src/Controller/EtudiantController.php
@@ -1,28 +1,52 @@
 <?php
 
 namespace App\Controller;
-use http\Env\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController ;
-use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EtudiantController extends AbstractController
-
 {
     #[Route('/etudiant', name: 'etudiant')]
-    public function index():Response
+    public function index(): Response
     {
-        return new Response("Bonjour");
+        return new Response("bonjour ");
     }
-   #[Route('/etudiant/{id}', name: 'affichage_etudiant', requirements: ['id' => '\d+'])]
-    public function affichegeetudiant($id):Response
-    {
-        return new Response("étudiant numéro".$id);
-    }
-    #[Route('/etudiant/name/{name}', name: 'etudiant_name')]
-    public function voirnom($name):Response
-    {
-        return $this->render('etudiant/voirnom.html.twig', ['name'=>$name]);
 
+    #[Route('/list', name: 'liste')]
+    public function listeEtudiant(){
+        $etudiants= array("ali","Med");
+
+        $modules = [
+            [
+                'id' => 1,
+                'nom' => 'Symfony',
+                'enseignant' => 'Ali',
+                'nbrHeures' => 42,
+                'date' => '12-12-2021'
+            ],
+            [
+                'id' => 2,
+                'nom' => 'Symfony',
+                'enseignant' => 'Aliii',
+                'nbrHeures' => 42,
+                'date' => '12-12-2021'
+            ]
+        ];
+
+        return $this->render('etudiant/list.html.twig',
+            array('etudiants'=>$etudiants, "listeModules"=>$modules));
+    }
+    #[Route('/affectation', name: 'Affectation')]
+    public function affectation()
+    {
+        return $this->render(view: 'etudiant/affectation.html.twig');
+    }
+    #[Route('/indexFils', name: 'index_fils')]
+    public function indexFils(): Response
+    {
+        // Rendu de la vue index.html.twig
+        return $this->render('etudiant/layout.html.twig');
     }
 }
